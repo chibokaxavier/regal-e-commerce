e69cd315daa1df0ba17d6a40c32f0f45e5dadf33
manlikexavy <chibokaxavier@gmail.com>
2023-03-09 08:08:20 -0800

    deleted products context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import Layout from "@/components/Layout";
-// import ProductProvider, { ProductContext } from "@/contexts/ProductContext";
 import SidebarProvider, { SidebarContext } from "@/contexts/SidebarContext";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
@@ -8,11 +7,9 @@ import { RecoilRoot } from "recoil";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SidebarProvider>
-    {/* <ProductProvider children={""}> */}
         <Layout>
           <Component {...pageProps} />
         </Layout>
-      {/* </ProductProvider> */}
     </SidebarProvider>
   );
 }
