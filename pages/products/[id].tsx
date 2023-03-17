894c7553ad42e097e49ac69024fbce7f13df195b
manlikexavy <chibokaxavier@gmail.com>
2023-03-17 07:12:55 -0700

    adjusted the screen size

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -24,24 +24,28 @@ const ProductDetails = () => {
       <Head>
         <title className="capitalize">Regal/{title}</title>
       </Head>
-      <div className="h-screen   lg:pb-32 pb-12 pt-[50px] flex items-center justify-center px-10 font-serif">
-        <div className="flex flex-col  lg:flex-row items-center justify-between pt-[100px] lg:pt-[180px]">
+      <div className="h-[1000px]  lg:pb-32 flex items-center justify-center px-10 font-serif">
+        <div className="flex flex-col  lg:flex-row items-center justify-between lg:pt-[180px]">
           <div className="flex flex-1 justify-center items-center mb-8 lg:mb-0 h-[500px] ">
-            <img src={image} alt="" className="max-w-[200px] h-full lg:max-w-sm" />
+            <img
+              src={image}
+              alt=""
+              className="max-w-[200px] h-full lg:max-w-sm"
+            />
           </div>
 
           <div className="flex-col flex h-[500px] lg:block  justify-center items-center space-y-8 lg:text-left lg:ml-[200px] lg:pt-0">
             <div>
-            <h1 className="text-[26px] font-light  max-w-[450px] uppercase">
-              {title}
-            </h1>
+              <h1 className="text-[26px] font-light  max-w-[450px] uppercase">
+                {title}
+              </h1>
             </div>
-          
-            <div className="text-xl font-medium  mb-6">
-               &#8358; {price}
-            </div>
+
+            <div className="text-xl font-medium  mb-6">&#8358; {price}</div>
             <p className="font-serif">{desc}</p>
-            <div className="p-4 text-green-500 border-2 w-[250px] flex items-center justify-center text-sm cursor-pointer">ORDER VIA WHATSAPP</div>
+            <div className="p-4 text-green-500 border-2 w-[250px] flex items-center justify-center text-sm cursor-pointer">
+              ORDER VIA WHATSAPP
+            </div>
             <div className="">
               <button
                 onClick={() => {
