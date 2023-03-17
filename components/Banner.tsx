615cad6a537060d7d65b022865aa6fb75a280562
manlikexavy <chibokaxavier@gmail.com>
2023-03-17 07:54:51 -0700

    added image

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -6,16 +6,23 @@ const Banner = () => {
   return (
     <>
       <div className="relative">
-        <div className=" bg-slate-600 lg:h-[86vh] lg:w-[100vw]  w-[100vw] h-[200px] ml-0 ">
-          <Image
+        <div className=" bg-slate-600 lg:h-[86vh] lg:w-[100vw]  w-[100vw] h-[200px] ml-0 bg-image">
+          {/* <Image
             src="https://images.pexels.com/photos/3189024/pexels-photo-3189024.jpeg?auto=compress&cs=tinysrgb&w=600"
             alt=""
             layout="fill"
             objectFit="cover"
             height={100}
             width={100}
-          />
-          {/* <img src="https://images.pexels.com/photos/1559086/pexels-photo-1559086.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" className='h-full w-full'  /> */}
+          /> */}
+          <style jsx>{`
+            .bg-image {
+              background-image: url(../images/model-image.jpg);
+              background-repeat: no-repeat;
+              background-size: cover;
+              background-position: top;
+            }
+          `}</style>
         </div>
       </div>
     </>
